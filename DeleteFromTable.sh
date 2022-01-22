594a07327c5f5fe745727bb41f20634d0e2c5b7f
sohila Muhammed <96821213+sohila-mo@users.noreply.github.com>
2022-01-22 13:23:54 -0800

    Add files via upload

---
+++ DeleteFromTable.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo -e "Enter Table Name:\c" 
+read TableName
+    if ! [[ -f Databases/$db_name/Data/$TableName ]]; then
+    echo "Table does exist"
+else
+    echo -e "Enter primary key: \c"
+    read recordNum
+    if ! [[ $recordNum =~ [`cut -d':' -f1 Databases/$db_name/Data/$TableName | grep -x $recordNum`] ]]; then
+	 	 echo "record not found"
+
+    else
+        sed -i /$recordNum/d Databases/$db_name/Data/$TableName
+        echo "record deleted successfully"
+
+    fi
+fi
